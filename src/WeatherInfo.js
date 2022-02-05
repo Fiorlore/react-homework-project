009cab96674e456722e4a6446b0325a7570da1a9
Fiorlore <87369031+Fiorlore@users.noreply.github.com>
2022-02-06 00:34:43 +0100

    updated layout and colors

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,18 +13,15 @@ export default function WeatherInfo(props) {
           <li>{props.data.description}</li>
         </ul>
         <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-              <div className="float-left">
-                <WeatherIcon code={props.data.icon} size={72} />
-              </div>
-  
-              <div className="float-left">
-                <WeatherTemperature celsius={props.data.temperature} />
-              </div>
+            <div className="col-2">
+                <WeatherIcon code={props.data.icon} size={76} />
             </div>
-          </div>
-          <div className="col-6">
+            <div className="col-4">
+                <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+            
+         
+          <div className="col-4">
             <ul>
               <li>Humidity: {props.data.humidity}%</li>
               <li>Wind: {props.data.wind} km/h</li>
